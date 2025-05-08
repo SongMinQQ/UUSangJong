1d8928f62ea691c9ccbd2ccbf8c2393b5867e477
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-05-08 14:33:05 +0900

    FEAT: 상세 페이지 레이아웃 구현

--- src/components/common/ContentItem.tsx
+++ src/components/common/ContentItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { memo, useCallback, useState } from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 type ItemDescriptionType = {
   title: string;
@@ -31,34 +32,36 @@ function ContentItem({ itemData }: ContentItemProp) {
   }, []);
 
   return (
-    <motion.div
-      key={itemData.id}
-      className="aspect-square relative cursor-pointer  overflow-hidden"
-      onHoverStart={handleMouseHover}
-      onHoverEnd={handleMouseLeave}
-    >
-      <motion.img
-        animate={{
-          filter: isHover ? "blur(2px) brightness(50%)" : "none",
-          transition: { duration: 0.3 },
-        }}
-        src={itemData.imageUrl}
-        alt={itemData.alt}
-        className="w-full h-full object-cover"
-      />
+    <Link href={`/board/${itemData.id}`}>
       <motion.div
-        className="absolute inset-0 flex items-center justify-center text-white text-lg font-semibold px-2"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: isHover ? 1 : 0 }}
-        transition={{ duration: 0.3 }}
+        key={itemData.id}
+        className="aspect-square relative cursor-pointer  overflow-hidden"
+        onHoverStart={handleMouseHover}
+        onHoverEnd={handleMouseLeave}
       >
-        <div className="absolute top-3 left-3 text-2xl">{itemData.detail.title}</div>
-        <div className="absolute top-1/2 left-3 -translate-y-1/2 text-left max-w-[70%]">
-          {itemData.detail.description}
-        </div>
-        <div className="absolute bottom-3 right-3 text-right">{itemData.detail.price}</div>
+        <motion.img
+          animate={{
+            filter: isHover ? "blur(2px) brightness(50%)" : "none",
+            transition: { duration: 0.3 },
+          }}
+          src={itemData.imageUrl}
+          alt={itemData.alt}
+          className="w-full h-full object-cover"
+        />
+        <motion.div
+          className="absolute inset-0 flex items-center justify-center text-white text-lg font-semibold px-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isHover ? 1 : 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <div className="absolute top-3 left-3 text-2xl">{itemData.detail.title}</div>
+          <div className="absolute top-1/2 left-3 -translate-y-1/2 text-left max-w-[70%]">
+            {itemData.detail.description}
+          </div>
+          <div className="absolute bottom-3 right-3 text-right">{itemData.detail.price}</div>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </Link>
   );
 }
 
