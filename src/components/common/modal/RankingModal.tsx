bd66d384515c7f51aa6a5d6bda1869f6c8278c55
ihyeeun <oioigl@naver.com>
2025-05-12 21:16:24 +0900

    FIX: 콘솔창 에러 해결 & merge conflict 해결

--- src/components/common/modal/RankingModal.tsx
+++ src/components/common/modal/RankingModal.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { DialogContent, DialogClose } from "@/components/ui/dialog";
+import { DialogContent, DialogClose, DialogTitle, DialogDescription } from "@/components/ui/dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { X } from "lucide-react"; // 닫기 아이콘
 import React from "react";
 import { UserRanking } from "@/types/rank";
@@ -24,11 +25,16 @@ const ModalRanking = ({ open }: ModalRankingProps) => {
     enabled: open,
   });
 
-  console.log("BUYER DATA : ", buyerData);
-  console.log("SELLER DATA : ", sellerData);
-
   return (
     <DialogContent className="border-none h-[75vh] bg-[#fefdf6] max-w-3xl w-full p-8 rounded-lg shadow-xl overflow-y-auto [&>button.absolute]:hidden">
+      {/* DialogContent에는 시각 장애인을 위한 screen reader가 내용을 이해할 수 있도록 DialogTitle과 DialogDescription이 필수. */}
+      <DialogTitle>
+        <VisuallyHidden>Ranking Modal</VisuallyHidden>
+      </DialogTitle>
+      <DialogDescription>
+        <VisuallyHidden>유저 랭킹을 보여주는 모달입니다.</VisuallyHidden>
+      </DialogDescription>
+
       <div className="absolute top-4 right-4">
         <DialogClose asChild>
           <button className="w-6 h-6 p-1 flex items-center justify-center text-gray-500 hover:text-black  cursor-pointer">
