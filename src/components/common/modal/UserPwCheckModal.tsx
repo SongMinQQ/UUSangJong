e48a55072beafabd33f4737d4aef1f960bb9b429
ihyeeun <oioigl@naver.com>
2025-05-13 19:39:09 +0900

    chore: PW 검증때 bool값 받아오는 형태 수정

--- src/components/common/modal/UserPwCheckModal.tsx
+++ src/components/common/modal/UserPwCheckModal.tsx
@@ -18,7 +18,8 @@ export default function ModalUserPwCheck({
   const [password, setPassword] = useState("");
 
   const handleSubmit = async () => {
-    const data = await handleApi(() => checkPassword(password));
+    const { data } = await handleApi(() => checkPassword(password));
+    console.log("비밀번호 체크 요청 결과 : ", data);
     if (data) {
       onSuccess();
       onClose();
