0fc8c2a70b8fb5c5bb658fa43eba9f36f68ee916
ihyeeun <oioigl@naver.com>
2025-05-15 21:08:59 +0900

    refactor: mypage, updatePage 코드 폴더별로 정리

--- src/types/userInfo.ts
+++ src/types/userInfo.ts
@@ -1,9 +1,11 @@
+// 읽기용 유저 정보
 export interface UserInfo {
   email: string;
   realname: string;
   nickname: string;
 }
 
+// 전송용 유저 정보
 export interface UserForm extends UserInfo {
-  password: string;
+  password?: string;
 }
