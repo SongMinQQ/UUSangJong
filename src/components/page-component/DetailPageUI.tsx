0cddc25e18e94e7ad02cad09bc3f58194dec0267
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-05-19 21:50:11 +0900

    FEAT: 화면 크기에 따른 content 배치

--- src/components/page-component/DetailPageUI.tsx
+++ src/components/page-component/DetailPageUI.tsx
@@ -72,8 +72,8 @@ export default function DetailPageUI() {
 
   return (
     <div className="w-full px-4 bg-[#fefdf6]">
-      <div className="flex flex-col xl:flex-row items-start gap-10 max-w-screen-xl mx-auto">
-        <div className="xl:mr-auto">
+      <div className="flex flex-col items-center justify-center xl:flex-row items-start gap-10 max-w-screen-xl mx-auto">
+        <div className="xl:w-auto xl:mx-0 w-full mx-auto">
           <ItemInfo images={postData?.images ?? []} />
           <ItemInfoTabs postId={postId} userId={postData.user_id} data={safeHtml} bids={bids} />
         </div>
